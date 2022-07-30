6035ae621f3e55bd4e6fa89f606477a5d9539b82
d9win <110292361+d9win@users.noreply.github.com>
2022-07-30 20:04:27 +0700

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '27c50dc65530825c75fb398f0efb9266',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://accusationcollegeload.com/27c50dc65530825c75fb398f0efb9266/invoke.js"></scr' + 'ipt>');
