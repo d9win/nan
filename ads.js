812ad54aeb7e2223a106a7516a37c8b30edb3b26
d9win <110292361+d9win@users.noreply.github.com>
2022-07-31 09:34:31 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,10 +1,8 @@
-<script type="text/javascript">
-	atOptions = {
-		'key' : '27c50dc65530825c75fb398f0efb9266',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
+atOptions = {
+		&#039;key&#039; : &#039;27c50dc65530825c75fb398f0efb9266&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://accusationcollegeload.com/27c50dc65530825c75fb398f0efb9266/invoke.js"></scr' + 'ipt>');
-</script>
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://accusationcollegeload.com/27c50dc65530825c75fb398f0efb9266/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
